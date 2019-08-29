b95f6e1ba49facea751db3acbd6c2cf2edccfb41
Jennifer Mealing <jennifermealingdesign@gmail.com>
2019-08-29 14:54:32 -0400

    whoops

--- working-on/long-doc-2/toc-custom.js
+++ working-on/long-doc-2/toc-custom.js
@@ -35,14 +35,14 @@ $('#toc').css({"maxHeight":($footerDiv.offset().top - $(window).scrollTop())+"px
 // write the toc position based on scroll position/ offset - but doesn't seem to be working in Safari...
 
 if ($menuButton.offset().top - $(window).scrollTop() === 0) {
-  $('#toc').addClass('.top');
+  $('#toc').addClass('top');
 } else {
-  $('#toc').removeClass('.top');
+  $('#toc').removeClass('top');
 }
   // $("#toc").css("top","10px");
   // } else {
   // $("#toc").css("top","");
-}
+// }
 
 // $('#toc').css({"top":($menuButton.offset())+"px"})
 // $('#toc').css({"top":($menuButton.offset().top)+"px"})
